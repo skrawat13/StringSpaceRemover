fac990bd4d0e01a8881f837136bf9b6fbc386cb0
Shiv Kumar <skrawat13@yahoo.com>
2022-04-07 11:21:22 +0530

    Initial

---
+++ Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Activities;
+using System.ComponentModel;
+using System.Text.RegularExpressions;
+
+namespace UIPathCustom_StringManupulation
+{
+    public class RemoveBlankLines:CodeActivity
+    {
+        [Category("input")]
+        
+        public InArgument<string> InputString  { get; set; }
+        [Category("Output")]
+
+        public OutArgument<string> Result { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+           
+        {
+            string strName = InputString.Get(context);
+           
+            strName= Regex.Replace(strName, @"^\s+$[\r\n]*", "", RegexOptions.Multiline);
+            string result = strName;
+
+            Result.Set(context, result);
+        }
+    }
+}
